1024c86911ce512d67a665b35f1cc838bae945c6
antrachowdhury <50897124+antrachowdhury@users.noreply.github.com>
2019-06-09 12:11:43 -0400

    Update week1.R

---
+++ CodePortfolio/week1.R
@@ -0,0 +1,17 @@
+# Complete the code for boolean_vector
+boolean_vector <- c(TRUE,FALSE,TRUE)
+
+# Adding two vector
+A_vector <- c(1, 2, 3)
+B_vector <- c(4, 5, 6)
+
+# Take the sum of A_vector and B_vector
+total_vector <- A_vector + B_vector
+
+# Define a new variable based on a selection
+poker_vector <- c(140, -50, 20, -120, 240)
+poker_midweek <- poker_vector[c(2:4)]
+poker_midweek
+
+# Calculate the average of a vector
+mean(poker_vector)
