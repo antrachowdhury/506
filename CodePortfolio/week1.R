797e34d8ef4c105f73452e4f597a1059642a3d34
antrachowdhury <50897124+antrachowdhury@users.noreply.github.com>
2019-06-09 12:22:44 -0400

    Update week1.R

--- CodePortfolio/week1.R
+++ CodePortfolio/week1.R
@@ -15,3 +15,21 @@ poker_midweek
 
 # Calculate the average of a vector
 mean(poker_vector)
+
+# Poker winnings from Monday to Friday:
+poker_vector <- c(140, -50, 20, -120, 240)
+days_vector <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+names(poker_vector) <- days_vector
+
+# Which days did you make money on poker?
+selection_vector <- poker_vector > 0
+
+# Select from poker_vector these days
+poker_winning_days <- poker_vector[selection_vector]
+poker_winning_days
+
+# Construct a matrix with 3 rows that contain the numbers 1 up to 9
+mat_1 <- matrix(1:9, byrow = TRUE, nrow=3)
+
+# Calculate rowSums for a matrix
+rowSums(mat_1)
